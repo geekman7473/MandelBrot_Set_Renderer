c7d3cdf8ee54f5caf08c148e4e3ff45788dfa01b
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2013-08-09 11:20:23 -0400

    Restructured folder tree

--- CPU Render Client/main.cpp
+++ CPU Render Client/main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
-#include "EasyBMP\EasyBMP.h"
+#include "depend\EasyBMP\EasyBMP.h"
 #include <stdio.h>
 #include <time.h>
 #include <assert.h>
-#include<cstdlib>
-#include<string>
-#include<sstream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include "depend\ttmath\ttmath\ttmath.h"
+
+//Project Build options specify the location of gmp.h
 #include "gmp.h"
-#include "ttmath\ttmath\ttmath.h"
 
 using namespace std;
 
-#define iterMax 10000
+#define iterMax 100
 
-typedef ttmath::Big<TTMATH_BITS(128), TTMATH_BITS(256)> LLDo;
+typedef ttmath::Big<TTMATH_BITS(32), TTMATH_BITS(1024)> LLDo;
 
 string intToString(long long int val);
 string doubleToString(long double val);
 BMP MandelFrameRender(LLDo xPan, LLDo yPan, long long maxIter, LLDo zoomFactor, long imgWidth, long imgHeight);
 
-int main()
-{
+int main(){
     /*
 mpf_t a,b,c;
 mpf_init2(a,NULL);
@@ -43,7 +44,7 @@ cout<<endl; */
 
 LLDo lol;
 lol = "-1.740062382579339905220844167065825638296641720436171866879862418461182919644153056054840718339483225743450008259172138785492983677893366503417299549623738838303346465461290768441055486136870719850559269507357211790243666940134793753068611574745943820712885258222629105433648695946003865";
-cout << lol << endl;
+cout << lol << endl << lol.ToString().length();
 system("PAUSE");
 //cin.get();
     //mpf_set()
@@ -55,16 +56,18 @@ system("PAUSE");
     LLDo zoomFactor =1;
     long long int totalZooms = 1;
     LLDo totalZoomFactNum = 0;
-while (zoomFactor < 100000000000000000000000000000000000){
+while (true){
 
-    LLDo xPan = -1.74006238257933990522084416706582563829664172043617;
+    LLDo xPan = 0;//-1.74006238257933990522084416706582563829664172043617;
     LLDo yPan = 0;//0.02817533977921104899241152114431950968753907674299;
 
-
-
+    zoomFactor = "1";
+    //for (int k = 0; k < 800; k++){
+    //    zoomFactor *= 10;
+    //}
     string filename = intToString(totalZooms) + " " + xPan.ToString().substr(0,8)+ "," + yPan.ToString().substr(0,8) + " AT " + zoomFactor.ToString() + " " +  intToString(test.TellWidth()) + "x" + intToString(test.TellHeight()) + "@" + intToString(max_iteration) + ".bmp";
     cout <<endl << filename << endl;
-    MandelFrameRender(xPan, yPan, iterMax, zoomFactor, 1920, 1080).WriteToFile(filename.c_str());
+    MandelFrameRender(xPan, yPan, iterMax, zoomFactor , 640, 480).WriteToFile(filename.c_str());
     zoomFactor  += ((pow(2,floor(log2(zoomFactor.ToDouble()))) / 30) * .75) * (1 + (((totalZooms +0) % 30) / 30));
     totalZoomFactNum += ((pow(2,floor(log2(zoomFactor.ToDouble()))) / 30) * .75) * (1 + ((totalZooms % 30) / 30));
     //cout <<endl <<totalZoomFactNum / totalZooms << endl;
