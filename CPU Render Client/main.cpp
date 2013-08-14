314663742f71bc38c0b14da6eab918fb2c9d394f
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2013-08-13 23:45:10 -0400

    GMP Built and properly installed

--- CPU Render Client/main.cpp
+++ CPU Render Client/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include "depend\EasyBMP\EasyBMP.h"
 #include <stdio.h>
 #include <time.h>
@@ -9,7 +10,7 @@
 #include "depend\ttmath\ttmath\ttmath.h"
 
 //Project Build options specify the location of gmp.h
-#include "gmp.h"
+#include "gmpxx.h"
 
 using namespace std;
 
@@ -17,35 +18,49 @@ using namespace std;
 
 typedef ttmath::Big<TTMATH_BITS(32), TTMATH_BITS(1024)> LLDo;
 
+struct workOrder_t {
+    //defines the type of work the client is required to do
+    //1 = full frame render
+    //2 = scan line
+    //3 = pixel
+    //0 = Null Work, when encountered these work allocations should be ignored
+    // -1 = Error, when encountered these work allocations should be ignored
+    short int workType;
+    //This string represents the coordinates of the center point of the current frame
+    //for pixel and scan line renders, the work assigned will be determined by the full
+    //frame determined by these coordinates, as well as dimensions and zoomFactor
+    string xCord, yCord;
+    //these value determine the width and height of the "frame of reference"
+    long long FrameWidth, FrameHeight;
+    //this value determines the zoomFactor for the frame of reference upon which the work is to be based
+    string zoomFactor;
+    //Determines the maximum iterations for the work
+    long long int maximumIterations;
+    //Finished work will be stored in this variable for transmission back to server
+    BMP completedWork;
+};
+
 string intToString(long long int val);
 string doubleToString(long double val);
-BMP MandelFrameRender(LLDo xPan, LLDo yPan, long long maxIter, LLDo zoomFactor, long imgWidth, long imgHeight);
+BMP MandelFrameRender(LLDo xPan, LLDo yPan, long long maxIter, LLDo zoomFactor, long long imgWidth, long long imgHeight);
 
 int main(){
-    /*
-mpf_t a,b,c;
-mpf_init2(a,NULL);
-mpf_init2(b,NULL);
-mpf_init2(c,NULL);
-
-mpf_set_str(a, "12348274342894733242342342.0",10);
-mpf_set_str(b,"-567832434238472963946287637846.0",10); //Decimal base
-
-mpf_add(c,a,b);
-
-cout<<"\nThe exact result is:";
-mpf_out_str(stdout,10,0,c);
-cout<<endl;
-
-mpf_abs(c,c);
-cout<<"The absolute value result is:";
-mpf_out_str(stdout,10,0,c);
-cout<<endl; */
-
 LLDo lol;
 lol = "-1.740062382579339905220844167065825638296641720436171866879862418461182919644153056054840718339483225743450008259172138785492983677893366503417299549623738838303346465461290768441055486136870719850559269507357211790243666940134793753068611574745943820712885258222629105433648695946003865";
 cout << lol << endl << lol.ToString().length();
 system("PAUSE");
+mpf_class aBigPO2 (1073741824, 1024);
+  aBigPO2*=aBigPO2; //2^60
+  aBigPO2*=aBigPO2; //2^120
+  aBigPO2*=aBigPO2; //2^240
+  aBigPO2*=aBigPO2; //2^480
+  aBigPO2*=aBigPO2; //2^960
+  aBigPO2*=aBigPO2; //2^1920
+
+  mpf_out_str(stdout,10,1000, aBigPO2.get_mpf_t());
+  system("PAUSE");
+
+
 //cin.get();
     //mpf_set()
 
@@ -76,7 +91,7 @@ while (true){
     return 0;
 }
 
-BMP MandelFrameRender(LLDo xPan, LLDo yPan, long long maxIter, LLDo zoomFactor, long imgWidth, long imgHeight){
+BMP MandelFrameRender(LLDo xPan, LLDo yPan, long long maxIter, LLDo zoomFactor, long long imgWidth, long long imgHeight){
     long long startT = time(NULL);
     BMP Frame;
     Frame.SetSize(imgWidth, imgHeight);
